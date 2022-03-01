c9ee5f98fa11080b5e9867e09441b4e8f3552afc
SathsaraniAththanayaka <e18030@eng.pdn.ac.lk>
2022-03-02 01:35:32 +0530

    Changed organizationRequest.php

--- UI/html/organizationRequest.php
+++ UI/html/organizationRequest.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html>
+
+<!DOCTYPE html> 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,7 +11,8 @@
 <body>
     <h1>Blood Bank Management System</h1>
     <h2>Your valuable donation saves 3 lives</h2>
-    
+   
+
     <div class="topNavigationBar">
         <a class="active" href="index.html">HOME</a>
     
@@ -29,6 +31,35 @@
         <a href="donor.html">WANT TO DONATE</a>
         <a href="aboutus.html">ABOUT US</a>
     </div>
-    
+    <div class="donorLogin">
+        <h1 align = "center">Organizations' request details</h1>
+        
+    </div>
 </body>
 </html>
+
+<?php
+     $hostname = "localhost";
+     $username = "root";
+     $password = "";
+     $databaseName = "BLOODBANK";
+
+    // connect to mysql
+    $connect = mysqli_connect($hostname, $username, $password, $databaseName);
+
+    $retriveFromOrganization = "SELECT organization_name,contact_number,address FROM organization WHERE request_status =1";
+    $retriveResult = mysqli_query($connect,$retriveFromOrganization);
+    
+    echo"<table border = '1'>";
+    echo"<tr><td>Organization name</td><td>Contact number</td><td>Address</td></tr>";
+
+    while($row = mysqli_fetch_assoc($retriveResult)){
+        echo"<tr><td>{$row['organization_name']}</td><td>{$row['contact_number']}</td><td>{$row['address']}</td></tr>";
+    }
+    echo"</table>";
+    
+
+    mysqli_close($connect);
+        
+
+?>
