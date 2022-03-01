4e37e063e137ddd0233323eff51dccbd6015e83e
Jayathri <e18283@eng.pdn.ac.lk>
2022-03-01 19:31:02 +0530

    updated acceptRequest.php (auto time-date slot updating)

--- UI/phpFiles/onlineDonorRequests.php
+++ UI/phpFiles/onlineDonorRequests.php
@@ -49,9 +49,36 @@
 ?>
 
 
-    <?php
-    require("navigationBar.php");
-    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/design.css">
+</head>
+<body>
+    <h1>Blood Bank Management System</h1>
+    <h2>Your valuable donation saves 3 lives</h2>
+    <div class="topNavigationBar">
+        <a class="active" href="index.html">HOME</a>
+    
+        <div class="dropdown">
+            <button class="dropbtn">LOOKING FOR BLOOD 
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+    
+              <a href="../phpFiles/bloodAvailability.php">Check Blood Availability</a>
+              <a href="../html/hospitalReq.html">Request Blood</a>
+              
+            </div>
+        </div> 
+    
+        <a href="donor.html">WANT TO DONATE</a>
+        <a href="aboutus.html">ABOUT US</a>
+    </div>
 
     
 
@@ -97,11 +124,11 @@
             url:"acceptRequest.php", //the page containing php script
             type: "POST", //request type
             data: {"id":id},
-            /*
+            
             success:function(result){
                     alert(result);
                 }
-            */
+            
             });
         }
 
