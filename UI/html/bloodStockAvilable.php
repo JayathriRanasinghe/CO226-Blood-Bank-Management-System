488bad087a51e8c43db6e333526602fca281265a
SathsaraniAththanayaka <e18030@eng.pdn.ac.lk>
2022-03-02 01:36:58 +0530

    Add bloodStockAvilable.php

---
+++ UI/html/bloodStockAvilable.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/design.css">
+</head>
+<body>
+    <h1>Blood Bank Management System</h1>
+    <h2>Your valuable donation saves 3 lives</h2>
+   
+
+    <div class="topNavigationBar">
+        <a class="active" href="index.html">HOME</a>
+    
+        <div class="dropdown">
+            <button class="dropbtn">LOOKING FOR BLOOD 
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+    
+              <a href="../phpFiles/bloodAvailability.php">Check Blood Availability</a>
+              <a href="../html/hospitalReq.html">Request Blood</a>
+              
+            </div>
+        </div> 
+    
+        <a href="donor.html">WANT TO DONATE</a>
+        <a href="aboutus.html">ABOUT US</a>
+    </div>
+    <div class="donorLogin">
+        <h1 align = "center">Blood availability in the blood stock</h1>
+        
+    </div>
+</body>
+</html>
